f4b925df1e1c74e45394722193e7bcf3f2b3c830
Caio Silva <hello@caiosilva.dev>
2020-01-31 20:46:27 +0000

    Procfile

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use('/api/recipes', recipesApiRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
+  console.log('ajsdinasiu')
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
