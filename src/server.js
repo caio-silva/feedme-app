d31b4346721de005fbcc635af2eee3f1d4eb6f13
Caio Silva <hello@caiosilva.dev>
2020-01-31 14:37:04 +0000

    Procfile

--- src/server.js
+++ src/server.js
@@ -19,13 +19,6 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// static folder setup
-// app.use(express.static(path.join(__dirname, 'public')));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
