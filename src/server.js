85145a25fb3dddbe666a265c329bfcc694333089
Caio Silva <hello@caiosilva.dev>
2020-01-31 22:53:47 +0000

    static.json added --fix

--- src/server.js
+++ src/server.js
@@ -32,7 +32,8 @@ app.use('/api/recipes', recipesApiRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
+    console.log(req)
     // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
