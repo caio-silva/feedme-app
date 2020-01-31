fb0c0e2569068703b04ff14f673125f3f952dfd8
Caio Silva <hello@caiosilva.dev>
2020-01-31 22:15:40 +0000

    static.json added --fix

--- src/server.js
+++ src/server.js
@@ -38,10 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -53,7 +50,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 app.listen(PORT, () => { console.log(`Server running and listening on port ${PORT}`) })
 //module.exports = app;
