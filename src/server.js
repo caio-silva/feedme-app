8d7f6cae5046960f4909d38e79e97e3b94aec16b
Caio Silva <hello@caiosilva.dev>
2020-02-01 17:45:45 +0000

    trying fix

--- src/server.js
+++ src/server.js
@@ -46,7 +46,7 @@ app.use(function (err, req, res, next) {
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
