ac04c4ee5bdbe41597c37eb67e5a501ceabbe417
Caio Silva <hello@caiosilva.dev>
2020-02-01 00:21:00 +0000

    static.json added --fix

--- src/server.js
+++ src/server.js
@@ -24,6 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// //////////////////////////
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+// //////////////////////
+
 // api routes setup
 app.use('/api/user', usersApiRouter);
 app.use('/api/settings', settingsApiRouter);
@@ -44,9 +48,9 @@ app.use(function (err, req, res, next) {
 
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
