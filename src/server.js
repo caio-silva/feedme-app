81396421b2f134f5d32d6c6ba1a3e8ed03b4956e
Caio Silva <hello@caiosilva.dev>
2020-02-01 19:57:43 +0000

    server.js prod

--- src/server.js
+++ src/server.js
@@ -49,8 +49,8 @@ if (process.env.NODE_ENV === 'production') {
   //set static folder
   // app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    res.sendFile(path.join(`${__dirname}/build/${req.params['0'].substr(1)}`));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    // res.sendFile(path.join(`${__dirname}/build/${req.params['0'].substr(1)}`));
   });
 }
 
